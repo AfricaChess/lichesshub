c40b5ccefeaf44c197466fb63dcf55d8c7589c64
bayo <bayokrapht@gmail.com>
2017-12-05 06:39:10 +0100

    get scores of games played

--- club/management/commands/get_result.py
+++ club/management/commands/get_result.py
@@ -1,5 +1,5 @@
 import requests
-import json
+import time
 
 from django.core.management.base import BaseCommand
 from django.conf import settings
@@ -11,37 +11,36 @@ class Command(BaseCommand):
     help = 'Get Result of game'
 
     def handle(self, *args, **kwargs):
-        games = Game.objects.filter(synced=False, error=False)
-        if not games:
-            self.stderr.write('No games to check')
-        else:
-            game = games[0]
-            url = '{}games/vs/{}/{}'.format(
-                settings.LICHESS_API_URL, game.white.handle, game.black.handle)
+        games = Game.objects.filter(
+            synced=False, error=False, game_id__isnull=False)
+        for game in games:
+            url = '{}game/{}'.format(settings.LICHESS_API_URL, game.game_id)
             self.stdout.write(url)
             resp = requests.get(url)
-            results = json.loads(resp.content)['currentPageResults']
+            data = resp.json()
 
-            #import pdb;pdb.set_trace()
-            for res in results:
-                if res['players']['white']['userId'] == game.white.handle and res['players']['black']['userId'] == game.black.handle:
-                    if res['status'] == 'draw':
-                        game.white_score = 1
-                        game.black_score = 1
+            # TODO: Verify that the user IDs are correct
+            if resp.status_code == 200:
+                if data['status'] == 'draw':
+                    game.white_score = 1
+                    game.black_score = 1
+                else:
+                    if data['winner'] == 'white':
+                        game.white_score = 2
+                        game.black_score = 0
                     else:
-                        if res['winner'] == 'white':
-                            game.white_score = 2
-                            game.black_score = 0
-                        else:
-                            game.white_score = 0
-                            game.black_score = 2
-                    game.synced = True
-                    game.save()
-                    break
+                        game.white_score = 0
+                        game.black_score = 2
+                game.synced = True
+                game.comment = '{} vs {}'.format(
+                    data['white']['userId'], data['black']['userId'])
+                game.save()
+                time.sleep(5)
             else:
                 game.error = True
                 game.save()
+                time.sleep(65)
 
             #import pdb;pdb.set_trace()
             #results = resp.content['paginator']['currentPageResults']
-            self.stdout.write(str(results))
+            self.stdout.write('done')
